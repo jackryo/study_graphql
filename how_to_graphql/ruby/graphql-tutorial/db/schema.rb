5e3c6e8123a0904eea98838c79d922d5f2f50c7f
takahashi <ryoutakachan@gmail.com>
2021-06-13 23:14:16 +0900

    bundle exec rails generate model User name email password_digest

--- how_to_graphql/ruby/graphql-tutorial/db/schema.rb
+++ how_to_graphql/ruby/graphql-tutorial/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_112708) do
+ActiveRecord::Schema.define(version: 2021_06_13_141131) do
 
   create_table "links", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
