ed1312aa745d9c6219f2d3bafc12579b14c8ec90
takahashi <ryoutakachan@gmail.com>
2021-06-20 17:01:47 +0900

    bundle exec rails generate model Vote link:references user:references && bundle exec rails db:migareate

--- how_to_graphql/ruby/graphql-tutorial/db/schema.rb
+++ how_to_graphql/ruby/graphql-tutorial/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_152755) do
+ActiveRecord::Schema.define(version: 2021_06_20_080044) do
 
   create_table "links", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "url"
@@ -29,5 +29,16 @@ ActiveRecord::Schema.define(version: 2021_06_13_152755) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "link_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
